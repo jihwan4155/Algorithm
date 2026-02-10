1c8962c491d0e3c0a28201de6d4a25af9c785cd3
jihwan/justin <jihwan4155@gmail.com>
2026-02-10 15:43:34 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 4일차 - 종이붙이기, Time: 96 ms, Memory: 61,568 KB -BaekjoonHub

---
+++ SWEA/D2/4869. ［파이썬 S／W 문제해결 기본］ 4일차 － 종이붙이기/［파이썬 S／W 문제해결 기본］ 4일차 － 종이붙이기.py
@@ -0,0 +1,11 @@
+def paper(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 3
+    return paper(n-1) + 2 * paper(n-2)
+
+T = int(input())
+for t in range(1, T+1):
+    n = int(input()) // 10
+    print(f'#{t}', paper(n))
