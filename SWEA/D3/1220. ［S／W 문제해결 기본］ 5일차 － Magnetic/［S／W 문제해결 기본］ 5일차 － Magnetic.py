5dfcd1b0c8809056e740b28643350e5d32a1d00e
jihwan/justin <jihwan4155@gmail.com>
2026-02-10 14:17:26 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 126 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,38 +1,22 @@
 for t in range(1, 11):
-    n = int(input())
+    N = int(input())
     arr = [list(map(int, input().split())) for _ in range(100)]
-
-    deadlock = 0
-    for i in range(100): # 열
-        stack = []
-        for j in range(100): # 행
-            mgn = arr[j][i]
-            if mgn != 0: # 0은 무시 0 이 아닌건 stack 추가
-                stack.append(mgn)
-        if len(stack) < 2: # 비거나 하나 있으면 패스
-            continue
-        
-        # 밖으로 나가는 자석들 제거
-        while len(stack) > 0 or stack[0] == 2 or stack[-1] == 1:
-            if stack[0] == 2:
-                stack.pop(0)
-            if stack[-1] == 1:
-                stack.pop()
-            if len(stack) < 2 or (stack[0] != 2 and stack[-1] != 1):
-                break
-        
-        # 다 나가거나 하나 남으면 패스
-        if len(stack) < 2:
-            continue
-
-        last = stack[0] # 1 2 1 2 형식으로 교착된 자석 + 1
-        check = 1
-        idx = 1
-        while idx < len(stack):
-            if stack[idx] != last:
-                last = stack[idx]
-                check += 1
-            idx += 1
-
-        deadlock += check//2 # 교착된 한 쌍의 자석들 총 수 // 2
-    print(f'#{t}', deadlock)
+    count = 0
+    for row in range(100):
+        for col in range(100):
+            if arr[row][col] == 1:
+                drow = row + 1
+                # 아래로 내린다. row + 1
+                while drow < 100:
+                    # 2 만나면 카운팅 후 break
+                    if arr[drow][col] == 2:
+                        count += 1
+                        break
+                    # 1 만나면 브레이크
+                    elif arr[drow][col] == 1:
+                        break
+                    # 아무것도 못만나면 계속 내려감
+                    else:
+                        drow += 1
+                        continue
+    print(f'#{t} {count}')
