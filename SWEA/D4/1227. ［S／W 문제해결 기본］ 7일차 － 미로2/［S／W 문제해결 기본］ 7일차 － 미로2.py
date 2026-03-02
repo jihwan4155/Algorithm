813959fbc6267a68059f37078f5d04a007018f6b
jihwan/justin <jihwan4155@gmail.com>
2026-03-02 18:00:04 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 100 ms, Memory: 60,800 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
@@ -1,6 +1,24 @@
 dcs = [0, 0, 1, -1]
 drs = [1, -1, 0, 0]
 
+def bfs(r, c):
+    q = [(r, c)]
+    visited[r][c] = 1
+
+    while q:
+        y, x = q.pop(0)
+        if arr[y][x] == '3':
+            return 1
+
+        for d in range(4):
+            dy = y + drs[d]
+            dx = x + dcs[d]
+            if 0 <= dy < 100 and 0 <= dx < 100 and visited[dy][dx] == 0 and arr[dy][dx] != '1':
+                visited[dy][dx] = 1
+                q.append((dy, dx))
+    return 0
+
+
 def dfs(r, c):
     visited[r][c] = 1
     stack = [(r, c)]
@@ -22,4 +40,5 @@ for _ in range(10):
     start = (1, 1)
     visited = [[0] * 100 for _ in range(100)]
 
-    print(f'#{t}', dfs(start[0], start[1]))
+    # print(f'#{t}', dfs(start[0], start[1]))
+    print(f'#{t}', bfs(start[0], start[1]))
