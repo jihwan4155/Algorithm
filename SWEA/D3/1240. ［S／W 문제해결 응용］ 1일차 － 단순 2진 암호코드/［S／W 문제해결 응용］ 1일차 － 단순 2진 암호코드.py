424b7ad63117a4471a1e61f9baea8bb5c2470405
jihwan/justin <jihwan4155@gmail.com>
2026-03-15 17:22:01 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 63 ms, Memory: 52,608 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,38 +1,54 @@
 mapping = {
-    '0001101': '0', '0011001': '1', '0010011':'2', '0111101': '3', '0100011':'4',
-    '0110001': '5', '0101111': '6', '0111011': '7', '0110111': '8', '0001011': '9',
+    '0001101': 0,
+    '0011001': 1,
+    '0010011': 2,
+    '0111101': 3,
+    '0100011': 4,
+    '0110001': 5,
+    '0101111': 6,
+    '0111011': 7,
+    '0110111': 8,
+    '0001011': 9,
 }
+
+def code(letter):
+    check = ''
+    for i in range(0, 56, 7):
+        check += str(mapping[letter[i:i+7]])
+    
+    even = 0
+    odd = 0
+    for i in range(len(check)):
+        if (i+1)%2 == 1:
+            odd += int(check[i])
+        else:
+            even += int(check[i])
+    
+    ans = odd*3 + even
+    if ans%10 == 0:
+        return odd+even
+    else:
+        return 0
+
 
 T = int(input())
 for t in range(1, T+1):
     n, m = map(int, input().split())
-    arr = [input() for _ in range(n)]
+    arr = set()
+    for _ in range(n):
+        a = input()
+        arr.add(a) # 중복 제거로 받기
 
-    code = ''
     flag = False
-    for i in range(n):
-        for j in range(m-1, -1, -1):
-            if arr[i][j] == '1':
-                code = arr[i][j-55:j+1]
+
+    for i in arr:
+        for idx in range(m-1, -1, -1):
+            if i[idx] == '1':
+                solve = code(i[idx-55:idx+1])
+                print(f'#{t}', solve)
                 flag = True
+                break
+            if idx < 55:
                 break
         if flag:
             break
-    
-    num = ''
-    for k in range(0, 56, 7):
-        num += mapping[code[k:k+7]]
-
-    even = 0
-    odd = []
-
-    for x in range(8):
-        if (x + 1) % 2 == 1:
-            odd.append(int(num[x]))
-        else:
-            even += int(num[x])
-
-    if (even + sum(odd) * 3) % 10 == 0:
-        print(f'#{t}', even+sum(odd))
-    else:
-        print(f'#{t}', 0)
