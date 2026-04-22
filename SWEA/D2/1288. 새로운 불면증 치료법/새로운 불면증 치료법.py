6bd2b26a6bc72371925b1f84feb7c759d39ea8ab
jihwan/justin <jihwan4155@gmail.com>
2026-04-22 15:39:35 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 78 ms, Memory: 59,520 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,28 +1,15 @@
-mask = [1] * 10
-
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    check = [0] * 10
-
-    num = 0
-    ans = N
-    idx = 1
-
-    flag = False
-    while True:
-        for i in str(ans):
-            if check[int(i)] == 0:
-                check[int(i)] = 1
-                num += 1
-                if num == 10:
-                    flag = True
-                    break
-        
-        if flag:
-            break
-
+    mask = 2**10-1 # bin = 1111111111
+    check = 0
+    idx = 0
+    while check < mask: # 1111111111 이 되기 전까지만. 되면 print로
         idx += 1
-        ans = N * idx
-    
-    print(f'#{tc} {ans}')
+        x = N * idx
+        while x != 0:
+            num = x % 10 # 한 자리씩 뽑기
+            x //= 10  # 한 자리씩 제거
+            check = (1 << num) | check #  지금 나온 숫자 [OR연산] 지금 까지 봤던 숫자 
+
+    print(f'#{tc} {N * idx}')
