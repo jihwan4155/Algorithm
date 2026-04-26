1fb5e850cc78f022398e69a4f182d0b7206372ed
jihwan/justin <jihwan4155@gmail.com>
2026-04-26 21:32:27 +0900

    [D3] Title: [S/W 문제해결 구현] 7일차 - 최소 비용, Time: 314 ms, Memory: 70,016 KB -BaekjoonHub

--- SWEA/D3/5250. ［S／W 문제해결 구현］ 7일차 － 최소 비용/［S／W 문제해결 구현］ 7일차 － 최소 비용.py
+++ SWEA/D3/5250. ［S／W 문제해결 구현］ 7일차 － 최소 비용/［S／W 문제해결 구현］ 7일차 － 최소 비용.py
@@ -1,32 +1,39 @@
-import heapq, math
-def dijkstra(r, c):
-    distance = [[math.inf]*n for _ in range(n)]
-    distance[r][c] = 0 # 시작점 거리 0
-    hq = [(0, r, c)]
-    
+import heapq
+
+def dijkstra(first):
+    dist = [[float('inf')] * n for _ in range(n)]
+    hq = []
+    dist[first[0]][first[1]] = 0
+    heapq.heappush(hq, (0, first))
+
     while hq:
-        fuel, row, col = heapq.heappop(hq)
-        if distance[row][col] < fuel:
+        distance, node = heapq.heappop(hq)
+
+        if dist[node[0]][node[1]] < distance:
             continue
-        for d in [[0, 1], [-1, 0], [0, -1], [1, 0]]:
-            dy = row + d[0]
-            dx = col + d[1]
+
+        for y, x in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
+            dy = node[0] + y
+            dx = node[1] + x
             if 0 <= dy < n and 0 <= dx < n:
-                altitude = arr[dy][dx]
-                exp_fuel = fuel + 1
-                if altitude > arr[row][col]:
-                    exp_fuel += altitude - arr[row][col]
-                # 다음 예상 연료 = 그 전까지 연료 + 높이 차이 (더 높다면) + 기본 연료 1 
-                if exp_fuel < distance[dy][dx]:
-                    heapq.heappush(hq, (exp_fuel, dy, dx))
-                    distance[dy][dx] = exp_fuel
-    
-    return distance[n-1][n-1]
+                high = 0
+                if arr[node[0]][node[1]] < arr[dy][dx]:
+                    high = arr[dy][dx] - arr[node[0]][node[1]] 
+                
+                weight = distance + high + 1
+
+                if weight < dist[dy][dx]:
+                    dist[dy][dx] = weight
+                    heapq.heappush(hq, (weight, (dy, dx)))
+        
+    return dist
+
 
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T+1):
     n = int(input())
-    # 출발 (0, 0), 도착 (n-1, n-1)
-    # 기본 이동 연료 1, 높이 다르면 높이 차이만큼 +
     arr = [list(map(int, input().split())) for _ in range(n)]
-    print(f'#{t}', dijkstra(0, 0))
+    start = (0, 0)
+    ret = dijkstra(start)
+
+    print(f'#{tc}', ret[n-1][n-1])
