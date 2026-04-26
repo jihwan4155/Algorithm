de5967ecf6359e9a459652bc9e85080a6b5f45d5
jihwan/justin <jihwan4155@gmail.com>
2026-04-26 17:15:14 +0900

    [D4] Title: 물놀이를 가자, Time: 3,589 ms, Memory: 102,752 KB -BaekjoonHub

--- SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.py
+++ SWEA/D4/10966. 물놀이를 가자/물놀이를 가자.py
@@ -1,34 +1,31 @@
 from collections import deque
+
 def bfs():
-    visited = [[0]*m for _ in range(n)]
     q = deque()
-    
     for i in range(n):
         for j in range(m):
             if arr[i][j] == 'W':
                 q.append((i, j))
-                visited[i][j] = 1
     
     while q:
         y, x = q.popleft()
-        for dy, dx in [[0, 1], [1, 0], [-1, 0], [0, -1]]:
-            dr = dy + y
-            dc = dx + x
-            if 0 <= dr < n and 0 <= dc < m and not visited[dr][dc]:
-                distance[dr][dc] = distance[y][x] + 1
-                visited[dr][dc] = 1
-                q.append((dr, dc))
-    
+        for dy, dx in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+            dr, dc = y + dy, x + dx
+            if 0 <= dr < n and 0 <= dc < m and arr[dr][dc] == 'L':
+                if visited[dr][dc] == 0:
+                    visited[dr][dc] = visited[y][x] + 1
+                    q.append((dr, dc))
                     
+
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T+1):
     n, m = map(int, input().split())
     arr = [input() for _ in range(n)]
-    cnt = 0
-    distance = [[0]*m for _ in range(n)]
+    visited = [[0]*m for _ in range(n)]
+    
     bfs()
+    cnt = 0
+    for row in visited:
+        cnt += sum(row)
     
-    for row in range(n):
-        cnt += sum(distance[row])
-    
-    print(f'#{t}', cnt)
+    print(f'#{tc} {cnt}')
