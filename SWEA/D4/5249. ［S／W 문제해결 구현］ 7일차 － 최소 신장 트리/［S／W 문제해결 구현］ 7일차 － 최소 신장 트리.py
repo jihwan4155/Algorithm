3917522a3c009d935c68562464e206fedc069eba
jihwan/justin <jihwan4155@gmail.com>
2026-04-26 21:11:08 +0900

    [D4] Title: [S/W 문제해결 구현] 7일차 - 최소 신장 트리, Time: 361 ms, Memory: 81,152 KB -BaekjoonHub

--- SWEA/D4/5249. ［S／W 문제해결 구현］ 7일차 － 최소 신장 트리/［S／W 문제해결 구현］ 7일차 － 최소 신장 트리.py
+++ SWEA/D4/5249. ［S／W 문제해결 구현］ 7일차 － 최소 신장 트리/［S／W 문제해결 구현］ 7일차 － 최소 신장 트리.py
@@ -1,32 +1,33 @@
 import heapq
-def prim(start):
-    hq = [(0, start)]
-    visited = [0] * (v+1)
-    min_weight = 0
-    
-    while hq:
-        weight, node = heapq.heappop(hq)
-        if visited[node]:
+
+def prim(v):
+    ans = 0
+    visited = [0] * (V+1)
+    q = []
+    heapq.heappush(q, (0, v))
+
+    while q:
+        weight, n = heapq.heappop(q)
+        if visited[n]:
             continue
-        
-        visited[node] = 1
-        min_weight += weight
-        
-        for w, next in graph[node]:
-            if not visited[next]:
-                heapq.heappush(hq, (w, next))
-                
-    return min_weight
+        visited[n] = 1
+        ans += weight
+        for nd, distance in node[n]:
+            if visited[nd]:
+                continue
+            heapq.heappush(q, (distance, nd))
+    
+    return ans
 
 
 T = int(input())
-for t in range(1, T+1):
-    # 0 - V 노드, E 간선
-    v, e = map(int, input().split())
-    graph = [[] for _ in range(v+1)]
-    for _ in range(e):
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    node = [[] for _ in range(V+1)]
+    for i in range(E):
         n1, n2, w = map(int, input().split())
-        graph[n1].append((w, n2))
-        graph[n2].append((w, n1))
+        node[n1].append((n2, w))
+        node[n2].append((n1, w))
     
-    print(f'#{t}', prim(0))
+    ret = prim(0)
+    print(f'#{tc}', ret)
