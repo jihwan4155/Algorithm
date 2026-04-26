bda2c5571a33ba05b42d2a4b086cf27cdf36f633
jihwan/justin <jihwan4155@gmail.com>
2026-04-26 15:36:36 +0900

    [D3] Title: 경로의 수, Time: 61 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D3/24220. 경로의 수/경로의 수.py
+++ SWEA/D3/24220. 경로의 수/경로의 수.py
@@ -1,30 +1,33 @@
-def dfs(v, lst):
-    global cnt
-
-    if lst:
-        if lst[-1] == g:
-            cnt += 1
-            return
-    
-    for w in graph[v]:
-        if w not in lst:
-            lst.append(w)
-            dfs(w, lst)
-            lst.pop()
-
 T = int(input())
-for t in range(1, T+1):
-    n, e = map(int, input().split())
-    way = list(map(int, input().split()))
-    s, g = map(int, input().split())
+
+def dfs(v, end):
+    global cnt 
+    visited[v] = True
+
+    if v == end:
+        cnt += 1
+        return
     
-    graph = [[] for _ in range(n+1)]
+    for w in node[v]:
+        if not visited[w]:
+            dfs(w, end)
+            visited[w] = False
+
+
+
+for tc in range(1, T+1):
+    N, E = map(int, input().split())
+    arr = list(map(int, input().split()))
+    S, G = map(int, input().split())
+
+    node = [[] for _ in range(N+1)]
+    for i in range(E):
+        s, g = arr[i*2], arr[i*2+1]
+        node[s].append(g)
     
-    for i in range(e):
-        start, end = way[i*2], way[i*2+1]
-        graph[start].append(end)
-    
-    visited = [0] * (n+1)
+    visited = [False] * (N+1)
+
     cnt = 0
-    dfs(s, [])
-    print(f'#{t}', cnt)    
+    dfs(S, G)
+
+    print(f'#{tc}', cnt)
